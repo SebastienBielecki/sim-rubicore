a02151db848907e79756215aad11478af77b00bd
Sebastien <seb_bielecki@hotmail.com>
2023-04-12 14:14:35 -0600

    Feat: Rubicore simulation initial commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Rubicore' });
+});
+
+router.post("/create-key", (req,res,next) => {
+  console.log("--- RUBICORE CREATE KEY TRIGGERED ----");
+  res.status(200).send({received: req.body})
+})
+
+
+module.exports = router;
