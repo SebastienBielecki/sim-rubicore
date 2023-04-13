ee26c3fb8d99e265bf07b403dfca066a1eca9c43
Sebastien <seb_bielecki@hotmail.com>
2023-04-12 20:46:36 -0600

    commit 4

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,14 @@ router.get('/rubicore2', function(req, res, next) {
   res.render('index', { title: 'Rubicore' });
 });
 
+router.get('/rubicore2/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/rubicore2/contacts', function(req, res, next) {
+  res.render('index', { title: 'contacts' });
+});
+
 router.post("/create-key", (req,res,next) => {
   console.log("--- RUBICORE CREATE KEY TRIGGERED ----");
   res.status(200).send({received: req.body})
